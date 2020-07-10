66a8796d0b31f7fbdbdd0dd8c38c82ab820b4df3
abesTaieb92 <abestaieb92@gmail.com>
2020-07-10 17:21:25 -0400

    my first comiit

---
+++ src/basics/HelloIntellij.java
@@ -0,0 +1,12 @@
+package basics;
+
+import java.net.StandardSocketOptions;
+import java.sql.SQLOutput;
+import java.util.SortedMap;
+
+public class HelloIntellij {
+    public static void main (String[]args){
+
+    }
+
+}
