a02257ab03d87e4eef69fc9268a5dd347445b8e3
abesTaieb92 <abestaieb92@gmail.com>
2020-07-11 20:16:45 -0400

    this is abes

--- src/basics/HelloIntellij.java
+++ src/basics/HelloIntellij.java
@@ -6,6 +6,9 @@ import java.util.SortedMap;
 
 public class HelloIntellij {
     public static void main (String[]args){
+        int ab=27;
+        int ag=23;
+        if(ab>ag) System.out.println("ab is the greatest");
 
     }
 
